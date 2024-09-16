41c6ae3af09d79ac176f24ce99144a1014f19f04
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-09-16 10:00:38 -0400

    add route for adding a purchase

--- routes/addPurchase.js
+++ routes/addPurchase.js
@@ -1,7 +1,11 @@
 import express, {Router} from 'express'; 
+import {mongoose} from 'mongoose'; 
+import PurchaseItem from '../mongoose/purchases/schema.js';
 const router = express.Router(); 
 
 router.post('/addPurchase', (req, res) =>{ 
+    console.log(typeof(req.body)); 
+    PurchaseItem.create(JSON.parse(req.body)); 
     console.log(req.body); 
 }); 
 export default router; 
